beca13389c8d3944aa16a5cc0d8a4c4a581cdb33
Peter <schmidt.multimedia@gmail.com>
2021-02-11 10:46:05 +0100

    03 Module -/*/:

--- script_01.js
+++ script_01.js
@@ -8,14 +8,39 @@
 4. Ausgabe in Konsole :: check
 */
 
+// Modul: Division a / b |  Test:
+ausgabe(dividieren(1,2));
+ausgabe(dividieren(1,-2));
+ausgabe(dividieren(10,0));
+function dividieren(a,b) {
+    
+    if (b != 0) {
+        // b ist nicht 0 / dividieren
+        return a / b;
+    } else {
+        // b ist 0 / warnmeldung ausgeben
+        return "Division durch 0 nicht OK!";
+    }
+    
+}
 
+// Modul: Multiplikation a * b |  Test:
+//ausgabe(multiplizieren(10,2));
+function multiplizieren(a,b) {
+   return a * b;
+}
+
+// Modul: Subtraktion a - b |  Test:
+//ausgabe(subtrahieren(0,2));
+function subtrahieren(a,b) {
+    return a - b;
+}
 
 // Modul: Addition a + b |  Test:
-//ausgabe(addieren(3,3));
+//ausgabe(addieren(3,2));
 function addieren(a,b) {
     return a + b;
 }
-
 
 // Modul: Konsolenausgabe |  Test:
 //ausgabe("hi");
